630fb6864dc0ddb800854b229414c9efe66f5255
Dhrumilshah777 <169030760+Dhrumilshah777@users.noreply.github.com>
2025-02-18 22:53:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,416 @@
+// Part 1
+
+// Question 1
+// {
+//     let result = prompt("Enter a number : ");
+//     if (result > 0) {
+//         console.log("Number is positive");
+//     } else {
+//         console.log("Number is negative");
+//     }
+//     console.log(result);
+// }
+
+// Question 2
+// {
+//     let user_age = prompt("Enter user age : ");
+//     if (user_age >= 18) {
+//         console.log(" eligible to vote ");
+//     }
+//     else {
+//         console.log("Not Eligible to vote ");
+//     }
+//     console.log(user_age);
+// }
+
+// Question 3
+// {
+//     let char = prompt("Enter character :");
+//     if (char >= "A" && char <= "Z") {
+//         console.log("Character are in Uppercase");
+//     } else {
+//         console.log("Characters are not in uppercase");
+//     }
+//     console.log(char);
+// }
+
+// Question 4
+// {
+//     let number = prompt("Enter a number :");
+//     if (number % 2 == 0) {
+//         console.log("Number is even");
+//     } else {
+//         console.log("Number is odd");
+//     }
+// }
+
+// Question 5
+// {
+//     let number = prompt("Enter a number :")
+//     if (number > 100) {
+//         console.log("Number is greater than 100");
+//     } else {
+//         console.log("Number is less than 100");
+//     }
+//     console.log(number);
+// }
+
+// Question 6 :
+// {
+//     let str = prompt("Enter a string");
+//     if(str[0] = "A"){
+//         console.log("The first letter is " , str[0]);
+//     }else{
+//         console.log("Not A");
+//     }
+//     console.log(str);
+// }
+
+// Question 7
+// {
+//     let number = prompt("Enter a number :");
+//     if (number % 5 == 0) {
+//         console.log("Multiple of 5");
+//     } else {
+//         console.log("Not a multiple of 5");
+//     }
+// }
+
+// Question 8
+// {
+//     let password = prompt("Enter a password");
+//     let confirmpassword = prompt("Confirm Password :");
+//     if (password == confirmpassword) {
+//         console.log("Welcome");
+//     } else {
+//         console.log("Password is incorrect");
+//     }
+// }
+
+// Question 9
+// {
+//     {
+//         let year = prompt("enter year");
+//         if (year % 4 == 0) {
+//             if (year % 100 == 0) {
+//                 if (year % 400 == 0) {
+//                     console.log("it is a leap year");
+//                 }
+//                 else {
+//                     console.log("its not a leap year");
+//                 }
+//             }
+//             else {
+//                 console.log("it is a leap year");
+//             }
+//         }
+//         else {
+//             console.log("its not a leap year");
+//         }
+//     }
+// }
+
+// Question 10
+// {
+//     let temp = prompt("Enter temperature :");
+//     if (temp > 30) {
+//         console.log("It's hot");
+//     }
+//     else {
+//         console.log("Cold");
+//     }
+// }
+
+// Part 2
+// Question 1
+// {
+//     let number = prompt("Enter a number :");
+//     if (number % 2 == 0) {
+//         console.log(number, "Number is even");
+//     } else {
+//         console.log(number, "Number is odd");
+//     }
+// }
+
+// Question 2
+// {
+//     let num1 = prompt("Enter number 1 :");
+//     let num2 = prompt("Enter number 2 :");
+//     if (num1 == num2) {
+//         console.log("Both the numbers are Equal");
+//     }
+//     else {
+//         if (num1 > num2) {
+//             console.log(num1, "num1 is greater");
+//         }
+//         else {
+//             console.log(num2, "num2 is greater");
+//         }
+//     }
+// }
+
+// Question 3
+// {
+//     let char = prompt("Enter char :");
+//     if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u" || char == "A" || char == "E" || char == "I" || char == "O" || char == "U") {
+//         console.log("It's a vowel");
+//     } else {
+//         if (char >= "a" && char <= "z") {
+//             console.log("CONSONANT");
+//         } else {
+//             console.log("Not an aplhabet");
+//         }
+//     }
+// }
+
+// Question 4
+// {
+//     let password = prompt("Enter password : ");
+//     let confirmpassword = prompt("Confirm Password :");
+//     if (password == confirmpassword) {
+//         console.log("Password is correct");
+//     } else {
+//         console.log("Password is incorrect");
+//     }
+// }
+
+// Question 5
+// {
+//     let user_age = prompt("Enter user age :");
+//     if (user_age >= 18) {
+//         console.log("Eliglibe for driving license");
+//     } else {
+//         console.log("Not eligible for driving license ");
+//     }
+// }
+
+// Question 6
+// {
+//     let number = prompt("Chal number nakh :");
+//     if (number >= 0) {
+//         console.log("Number is positive");
+//     } else {
+//         console.log("Number ")
+//     }
+// }
+
+// Question 7
+// {
+//     let person = prompt("Enter person age : ");
+//     if (person >= 18) {
+//         console.log("Adult che");
+//     } else {
+//         console.log("Minor che ");
+//     }
+// }
+
+// Question 8
+// {
+//     {
+//         let str = prompt("Enter a string:");
+//         if (str === "") {
+//             console.log("The string is empty.");
+//         } else {
+//             console.log("The string is not empty.");
+//         }
+//     }
+// }
+
+// Question 9
+// {
+//     let marks = prompt("Enter marks :");
+//     if (marks >= 40) {
+//         console.log("Jalsa kar Pass thai gyo  ");
+//     } else {
+//         console.log("Mehnat kar fail thai gyo ");
+//     }
+// }
+
+// Question 10
+// {
+//     let day = prompt("Enter day : ");
+//     if (day == "Saturday" || day == "saturday" || day == "SATURDAY") {
+//         console.log("Aaj Shanivar hei");
+//     } else {
+//         console.log("It's a weekday");
+//     }
+// }
+
+
+// Part 3
+// Question 1
+// {
+//     let number = prompt("Enter a number : ");
+//     if (number > 0) {
+//         console.log("Number is positive");
+
+//     } else {
+//         if (number < 0) {
+//             console.log("Number is negative");
+//         } else {
+//             console.log("Number is zero");
+//         }
+//     }
+// }
+
+// Question 2
+
+// {
+//     {
+//         let year = prompt("enter year");
+//         if (year % 4 == 0) {
+//             if (year % 100 == 0) {
+//                 if (year % 400 == 0) {
+//                     console.log("it is a leap year");
+//                 }
+//                 else {
+//                     console.log("its not a leap year");
+//                 }
+//             }
+//             else {
+//                 console.log("it is a leap year");
+//             }
+//         }
+//         else {
+//             console.log("its not a leap year");
+//         }
+//     }
+// }
+
+// Question 3
+// {
+//     let age = prompt("Enter age :");
+//     if (age > 0 && age <= 12) {
+//         console.log("Child");
+//     } else {
+//         if (age >= 13 && age <= 19) {
+//             console.log("Teen");
+//         } else {
+//             console.log("Adult");
+//         }
+//     }
+// }
+
+
+// Question 4
+// {
+//     let side1 = prompt("Enter side 1 :");
+//     let side2 = prompt("Enter side 2 :");
+//     let side3 = prompt("Enter side 3 :");
+//     if (side1 === side2 && side2 === side3) {
+//         console.log("Equiletral");
+//     }
+//     else {
+//         if (side1 === side2 || side2 === side3 || side1 === side3) {
+//             console.log("Isoceles");
+//         } else {
+//             console.log("Scalene");
+//         }
+//     }
+// }
+
+// Question 5
+// {
+//     let marks = prompt("Enter marks : ");
+//     if (marks >= 80 && marks <= 100) {
+//         console.log("Distinction");
+//     } else {
+//         if (marks >= 40 && marks < 80) {
+//             console.log("Pass");
+//         } else {
+//             console.log("Fail");
+//         }
+//     }
+// }
+
+
+// Question 6
+// {
+//     let char = prompt("Enter char :");
+//     if (char >= "A" && char <= "Z") {
+//         console.log("Uppercase");
+//     } else {
+//         if (char >= "a" && char <= "z") {
+//             console.log("Lowecase");
+//         } else {
+//             console.log("Digit");
+//         }
+//     }
+// }
+// Question 7
+
+// {
+//     let num = prompt("enter number");
+//     if (num % 3 == 0) {
+//         if (num % 5 == 0) {
+//             console.log("it is div by both 3 and 5");
+//         }
+//         else {
+//             console.log("div by 3 ONLY");
+//         }
+//     } else {
+//         if (num % 5 == 0) {
+//             console.log("div by 5 ONLY");
+//         }
+//         else {
+//             console.log("not by 3 OR 5");
+//         }
+//     }
+// }
+
+
+// Question 8
+// {
+//     let number1 = Number(prompt("Enter number 1 :"));
+//     let number2 = Number(prompt("Enter number 2 :"));
+//     let number3 = Number(prompt("Enter number 3 :"));
+//     if (number1 > number2 && number1 > number3) {
+//         console.log("Number 1 is greater");
+
+//     } else {
+//         if (number2 > number3) {
+//             console.log("Number 2 is greater");
+//         } else {
+//             console.log("Number 3 is greater");
+//         }
+//     }
+// }
+
+
+// Question 9
+// {
+//     let marks = prompt("Enter marks :");
+//     if (marks >= 90) {
+//         console.log("Grade A");
+
+//     } else {
+//         if (marks >= 80 && marks <= 89) {
+//             console.log("Grade B");
+//         } else {
+//             if (marks >= 70 && marks <= 79) {
+//                 console.log("Grade C");
+//             } else {
+//                 if (marks >= 60 && marks <= 69) {
+//                     console.log("Grade D");
+//                 } else {
+//                     console.log("Fail");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Question 10
+// {
+//     let income = prompt("Ente income :");
+//     let credit_score = prompt("Enter score :");
+//     if (income >= 5000) {
+//         if (credit_score >= 500) {
+//             console.log("Eligible for a loan");
+//         } else {
+//             console.log("Not eligible for a loan");
+//         }
+//     } else {
+//         console.log("Not eligible for a loan");
+//     }
+// }
